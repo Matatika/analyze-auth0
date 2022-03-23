24497f75bd3b94ce86119b943de68d99847ba16f
danielpdwalker <danielpdwalker@gmail.com>
2022-03-23 15:58:33 +0000

    Bumped package version to 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="analyze-auth0",
-    version="0.4.0",
+    version="0.4.1",
     description="Meltano project file bundle of Matatika datasets for tap-auth0",
     packages=find_packages(),
     package_data={
